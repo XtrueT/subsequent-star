8cbd5bb58d0785e1e4cd88a96f2956007c9b5590
XtrueT <xierongfeng@outlook.com>
2024-08-16 22:13:53 +0800

    add post

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,8 +11,8 @@ export default {
           sm: '640px',
           md: '768px',
           lg: '1024px',
-          xl: '1280px',
-          '2xl': '1280px',
+          xl: '1024px',
+          '2xl': '1024px',
         },
       }),
       typography: {
